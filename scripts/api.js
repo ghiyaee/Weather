ad40c97714cd11a391010c153ec7420f88634a79
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-02-16 14:37:00 +0330

    update weather

--- scripts/api.js
+++ scripts/api.js
@@ -2,7 +2,6 @@ const key = 'xupAMNmNfGgKkP9gb4wjqDrC0rGQSTsH';
 const getWeather = async (id) => {
     const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${id}?apikey=${key}`;
-
     const response = await fetch(base + query);
     const data = await response.json();
     return data[0];
