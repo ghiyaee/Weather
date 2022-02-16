c2dda190e97c7df2748931130e248b01464d0fda
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-02-16 13:54:33 +0330

    weather

---
+++ scripts/dom.js
@@ -0,0 +1,55 @@
+const input = document.querySelector('.chang-loction');
+const delails = document.querySelector('.text-details');
+const card = document.querySelector('.card');
+const time = document.querySelector('img.time');
+const icone=document.querySelector('.icon img')
+console.log(icone);
+const updateUl = (city) => {
+     console.log(city);
+    const citydets = city.cityhttp;
+    const weather = city.weather;
+
+    const icones = `imag/icons/${weather.WeatherIcon}.svg`;
+    icone.setAttribute('src',icones)
+    // const { citydets, weather } = city;
+    let timesrc = null;
+    if (weather.IsDayTime) {
+        timesrc='imag/day.svg'
+    } else {
+        timesrc = 'imag/night.svg'
+    }
+
+    time.setAttribute("src",timesrc)
+
+
+
+
+
+    delails.innerHTML = `<h5 class="city-name">${citydets.EnglishName}</h5>
+                <div class="weather-condition">${weather.WeatherText}</div>
+                <div class="display">
+                    <span>${weather.Temperature.Metric.Value}</span>
+                    <span>&deg;C</span>
+                </div>`;
+    card.classList.remove('hiden')
+}
+
+const newcity = async (city) => {
+    const cityhttp = await getCity(city);
+    const weather = await getWeather(cityhttp.Key);
+
+    return {
+        cityhttp,
+         weather
+    };
+}
+
+input.addEventListener('submit', (e)=>{
+    
+    const city = input.city.value.trim();
+    input.reset();
+    e.preventDefault();
+    newcity(city)
+        .then(city => updateUl(city))
+        .catch(err => console.log(err))
+})
